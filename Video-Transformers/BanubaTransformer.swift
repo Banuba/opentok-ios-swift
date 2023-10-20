917a3fd5db601ddda7c7659f8c8ae1ec9798046b
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-20 13:21:03 +0300

    added new api

--- Video-Transformers/BanubaTransformer.swift
+++ Video-Transformers/BanubaTransformer.swift
@@ -10,24 +10,25 @@ class BanubaTransformer: NSObject, OTCustomVideoTransformer {
     public override init() {
     
         if !BanubaTransformer.intialized {
-            BanubaSdkManager.initialize(
-                resourcePath: [
-                    Bundle.main.bundlePath + "/bnb-resources",
-                    Bundle.main.bundlePath + "/effects"
-                ],
-                clientTokenString: kClientToken
-            )
+            var resourcePaths = [
+                Bundle.main.bundlePath + "/bnb-resources",
+                Bundle.main.bundlePath + "/effects",
+            ]
+            for f in Bundle.allFrameworks {
+                if let b = f.bundleIdentifier, b.starts(with: "banuba.sdk.BNB") {
+                    resourcePaths.append(f.bundlePath + "/bnb-resources")
+                }
+            }
+            BanubaSdkManager.initialize(resourcePath: resourcePaths, clientTokenString: kClientToken)
             BanubaTransformer.intialized = true
         }
     }
     
     public func loadEffect(name: String) {
         effectName = name
-        if oep != nil {
-            oep?.loadEffect(name)
-        }
+        _ = player?.load(effect: effectName)
     }
-    
+
     public static func deinitialize() {
         if BanubaTransformer.intialized {
             BanubaSdkManager.deinitialize()
@@ -36,16 +37,49 @@ class BanubaTransformer: NSObject, OTCustomVideoTransformer {
     }
     
     func transform(_ videoFrame: OTVideoFrame) {
-        let width  = Int(videoFrame.format?.imageWidth ?? 0)
-        let height = Int(videoFrame.format?.imageHeight ?? 0)
-        createOepIfRequiered(width, height)
-        var bnbFormat = createBnbFormat(videoFrame)
-         
+        self.lastVideoFrame = videoFrame
+
+        if player == nil {
+            input = BNBSdkApi.Stream()
+            output = BNBSdkApi.PixelBufferYUV(onPresent: {(pixelBuffer) -> Void in
+                CVPixelBufferLockBaseAddress(pixelBuffer!, .readOnly)
+
+                guard let frame = self.lastVideoFrame else {return}
+
+                let y = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer!, 0)!.assumingMemoryBound(to: UInt8.self)
+                let u = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer!, 1)!.assumingMemoryBound(to: UInt8.self)
+                let v = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer!, 2)!.assumingMemoryBound(to: UInt8.self)
+                let strideY = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer!, 0)
+                let strideU = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer!, 1)
+                let strideV = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer!, 2)
+
+                let frameWidth = Int(frame.format?.imageWidth ?? 0)
+                let frameHeight = Int(frame.format?.imageHeight ?? 0)
+                for h in 0 ..< frameHeight {
+                    memcpy(frame.getPlaneBinaryData(0) + h * Int(frame.getPlaneStride(0)), y + strideY * h, frameWidth)
+                }
+                for h in 0 ..< frameHeight / 2 {
+                    memcpy(frame.getPlaneBinaryData(1) + h * Int(frame.getPlaneStride(1)), u + strideU * h, frameWidth / 2)
+                    memcpy(frame.getPlaneBinaryData(2) + h * Int(frame.getPlaneStride(2)), v + strideV * h, frameWidth / 2)
+                }
+
+                CVPixelBufferUnlockBaseAddress(pixelBuffer!, .readOnly)
+            }, pixelFormatType: .k420YpCbCr8PlanarFullRange)
+
+            player = Player()
+            _ = player?.load(effect: effectName)
+            player?.renderMode = .manual
+            player?.use(input: input!, outputs: [output!])
+            player?.play()
+        }
+
         var planes = [
             videoFrame.planes?.pointer(at: 0),
             videoFrame.planes?.pointer(at: 1),
             videoFrame.planes?.pointer(at: 2)
         ]
+        let width  = Int(videoFrame.format?.imageWidth ?? 0)
+        let height = Int(videoFrame.format?.imageHeight ?? 0)
         var widths = [width, width / 2, width / 2]
         var heights = [height, height / 2, height / 2]
         var strides = [
@@ -53,7 +87,7 @@ class BanubaTransformer: NSObject, OTCustomVideoTransformer {
             Int(videoFrame.getPlaneStride(1)),
             Int(videoFrame.getPlaneStride(2))
         ]
-        
+
         var otBuffer: CVPixelBuffer?;
         assert(CVPixelBufferCreateWithPlanarBytes(
             kCFAllocatorDefault,
@@ -72,72 +106,16 @@ class BanubaTransformer: NSObject, OTCustomVideoTransformer {
             nil,
             &otBuffer
         ) == kCVReturnSuccess)
-        let processResult = oep?.processImage(otBuffer!, with: &bnbFormat)
-        
-        CVPixelBufferLockBaseAddress(processResult!, .readOnly)
-        
-        let y = CVPixelBufferGetBaseAddressOfPlane(processResult!, 0)!.assumingMemoryBound(to: UInt8.self)
-        let u = CVPixelBufferGetBaseAddressOfPlane(processResult!, 1)!.assumingMemoryBound(to: UInt8.self)
-        let v = CVPixelBufferGetBaseAddressOfPlane(processResult!, 2)!.assumingMemoryBound(to: UInt8.self)
-        let strideY = CVPixelBufferGetBytesPerRowOfPlane(processResult!, 0)
-        let strideU = CVPixelBufferGetBytesPerRowOfPlane(processResult!, 1)
-        let strideV = CVPixelBufferGetBytesPerRowOfPlane(processResult!, 2)
-        
-        for h in 0 ..< height {
-            memcpy(videoFrame.getPlaneBinaryData(0) + h * strides[0], y + strideY * (height - h - 1), width)
-        }
-        for h in 0 ..< height / 2 {
-            memcpy(videoFrame.getPlaneBinaryData(1) + h * strides[1], u + strideU * (height / 2 - h - 1), width / 2)
-            memcpy(videoFrame.getPlaneBinaryData(2) + h * strides[2], v + strideV * (height / 2 - h - 1), width / 2)
-        }
-        
-        CVPixelBufferUnlockBaseAddress(processResult!, .readOnly)
+
+        input?.push(pixelBuffer: otBuffer!)
+        _ = player?.render()
     }
-    
-    private func createOepIfRequiered(_ width:Int, _ height:Int) {
-        if oep == nil {
-            oep = BNBOffscreenEffectPlayer(
-                effectWidth: UInt(width),
-                andHeight: UInt(height),
-                manualAudio: true
-            )
-            playerWidth = width
-            playerHeight = height
-            oep?.loadEffect(effectName)
-        } else {
-            if playerWidth != width || playerHeight != height {
-                oep?.surfaceChanged(UInt(width), withHeight: UInt(height))
-                playerWidth = width
-                playerHeight = height
-            }
-        }
-    }
-    
-    private func createBnbFormat(_ otFrame:OTVideoFrame) -> EpImageFormat {
-        let bnbOrientation = {(otOrientation:OTVideoOrientation)->EPOrientation in
-            switch otOrientation {
-                case .up: return .angles0
-                case .left: return .angles90
-                case .right: return .angles270
-                case .down: return .angles180
-                @unknown default: return .angles0
-            }
-        }(otFrame.orientation)
-    
-        return EpImageFormat(
-            imageSize: CGSize(width: CGFloat(otFrame.format?.imageWidth ?? 0), height: CGFloat(otFrame.format?.imageHeight ?? 0)),
-            orientation: bnbOrientation,
-            resultedImageOrientation: bnbOrientation,
-            isMirrored: true,
-            needAlphaInOutput: false,
-            overrideOutputToBGRA: false,
-            outputTexture: false
-        )
-    }
-    
-    private var oep: BNBOffscreenEffectPlayer?
+
+    private var player: BNBSdkApi.Player?
+    private var input: BNBSdkApi.Stream?
+    private var output: BNBSdkApi.PixelBufferYUV?
+    private var lastVideoFrame: OTVideoFrame? = nil
+
     private var effectName: String = ""
     private static var intialized = false
-    private var playerWidth = 0
-    private var playerHeight = 0
 }
